69d9b560953f7ccae57eda4768efd948f2e12101
Mikhail Sementsov <fewebdev@gmail.com>
2018-10-15 21:33:39 +0300

    done

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,20 +1,5 @@
-let img = document.querySelector("img"),
-    span = document.querySelector("span");
-
-function posToFilter() {
-  let controls = {
-    blur: document.querySelector("input#blur").value,
-    size: document.querySelector("input#spacing").value,
-    color: document.querySelector("input#base").value
-  };
-
-  img.style.filter = `blur(${controls.blur}px)`;
-  img.style.padding = `${controls.size}px`;
-  span.style.color = img.style.backgroundColor = controls.color;
-}
-
 document.addEventListener("input", e => {
-  if (e.target.tagName === "INPUT") posToFilter();
+  if (e.target.tagName === "INPUT") {
+    document.body.style.setProperty(`--${e.target.name}`, e.target.value);
+  }
 });
-
-posToFilter();
